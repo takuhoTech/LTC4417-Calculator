979ec7e7419f27e6bb80cfe0a33205fee39cfec5
takuhoTech <109070168+takuhoTech@users.noreply.github.com>
2023-12-17 13:46:17 +0900

    add 2-cell func

--- src/main.py
+++ src/main.py
@@ -19,9 +19,9 @@ def btn_click():
    R4 = R1 + R2
    if Rhys != 0:
       Ihys = 63.0/(1000.0*Rhys)
-      txt[11].delete(0, tkinter.END)
+      txt[15].delete(0, tkinter.END)
       if (Ihys < 0.000000050) or (Ihys > 0.000000500):
-         txt[11].insert(0,"Ihys range error")
+         txt[15].insert(0,"Ihys range error")
       OVhys = Ihys*((R12*R123/R1)+R2+R3)
       UVhys = Ihys*((R23*R123/R4)+R3)
    else:
@@ -35,15 +35,23 @@ def btn_click():
    txt[8].delete(0, tkinter.END)
    txt[9].delete(0, tkinter.END)
    txt[10].delete(0, tkinter.END)
-
    txt[7].insert(0,UV)
-   txt[8].insert(0,str(UVhys))
+   txt[8].insert(0,UVhys)
    txt[9].insert(0,OV)
-   txt[10].insert(0,str(OVhys))
+   txt[10].insert(0,OVhys)
+
+   txt[11].delete(0, tkinter.END)
+   txt[12].delete(0, tkinter.END)
+   txt[13].delete(0, tkinter.END)
+   txt[14].delete(0, tkinter.END)
+   txt[11].insert(0,UV/2.0)
+   txt[12].insert(0,UVhys/2.0)
+   txt[13].insert(0,OV/2.0)
+   txt[14].insert(0,OVhys/2.0)
 
 # 画面作成
 tki = tkinter.Tk()
-tki.geometry('700x450')
+tki.geometry('1000x450')
 tki.title('LTC4417のOV/UVしきい値とヒステリシス電圧の計算機')
 # ラベル
 tkinter.Label(text='R1').place(x=30, y=70)
@@ -67,11 +75,12 @@ tkinter.Label(text='Error').place(x=30, y=400)
 
 i = 0
 while i <= 3:
-   tkinter.Label(text='V').place(x=250, y=280+30*i)
+   tkinter.Label(text='/2=').place(x=250, y=280+30*i)
+   tkinter.Label(text='V').place(x=410, y=280+30*i)
    i = i + 1
 
 # テキストボックス
-txt = [0]*12
+txt = [0]*16
 
 txt[1] = tkinter.Entry(width=20)
 txt[1].place(x=120, y=70)
@@ -95,8 +104,17 @@ txt[9].place(x=120, y=340)
 txt[10] = tkinter.Entry(width=20)
 txt[10].place(x=120, y=370)
 
-txt[11] = tkinter.Entry(width=60)
-txt[11].place(x=120, y=400)
+txt[11] = tkinter.Entry(width=20)
+txt[11].place(x=280, y=280)
+txt[12] = tkinter.Entry(width=20)
+txt[12].place(x=280, y=310)
+txt[13] = tkinter.Entry(width=20)
+txt[13].place(x=280, y=340)
+txt[14] = tkinter.Entry(width=20)
+txt[14].place(x=280, y=370)
+
+txt[15] = tkinter.Entry(width=60)
+txt[15].place(x=120, y=400)
 
 # ボタン
 btn = tkinter.Button(tki, text='計算', command=btn_click)
@@ -113,7 +131,7 @@ img_width, img_height = img.size
 
 canvas = tkinter.Canvas(tki, width=img_width, height=img_height)        # 画像表示エリアの作成
 #canvas.pack()
-canvas.place(x=330, y=60)
+canvas.place(x=500, y=60)
 canvas.create_image(0, 0 , anchor = tkinter.NW, image=tk_img)        # 画像表示
 
 tki.mainloop()
